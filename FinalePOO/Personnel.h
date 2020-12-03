bb8e66fd6fd982becbf5a9a14e8e8bec13368743
Silvio <Silvio@DESKTOP-GMKS6CP>
2020-12-03 15:29:41 +0100

    Ajouter Supprimer Personnel

--- FinalePOO/Personnel.h
+++ FinalePOO/Personnel.h
@@ -5,8 +5,8 @@ ref class Personnel
 	/*Rappel des attributs : K_ID, Nom, Prenom, Date, FK_ID
 puis  liaison à Adresse : K_ID, numero, typeDeVoie, LibelleDeVoie, Ville, codePostal, FK_IDClient*/
 public :
-	void ajouter();
-	void supprimer();
+	void ajouter(System::String^, System::String^, System::String^, System::String^, System::String^, System::String^, System::String^, System::String^, System::String^);
+	void supprimer(System::String^);
 	void afficher();
 	void modifier();
 }; 
