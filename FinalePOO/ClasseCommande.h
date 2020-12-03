fb6c72f33660d545530e0fa913262db0069fd1eb
Silvio <Silvio@DESKTOP-GMKS6CP>
2020-12-03 20:40:30 +0100

    cc

--- FinalePOO/ClasseCommande.h
+++ FinalePOO/ClasseCommande.h
@@ -2,8 +2,6 @@
 ref class Commande
 {
 public :
-	void ajouterPanier(System::String^ NomProduit, System::String^ ID, System::String^);
-	void retirerPanier();
 	void ajouterCommande(System::String^ dateE, System::String^ dateL, System::String^ NDC);
 	void panierdynamiqueAjout(System::String^ ID, System::String^ Reference, System::String^);
 	void panierdynamiqueRetirer(System::String^, System::String^);
