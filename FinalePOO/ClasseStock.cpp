9037a8001253e534630fd16c1c73ab219dbea5ac
Silvio <Silvio@DESKTOP-GMKS6CP>
2020-12-03 05:11:33 +0100

    UPDATE Stock

--- FinalePOO/ClasseStock.cpp
+++ FinalePOO/ClasseStock.cpp
@@ -5,61 +5,6 @@ Stock::~Stock()
 
 }
 
-int Stock::getID()
-{
-    return IDproduit;
-}
-
-System::String^ Stock::getNom()
-{
-    return nomProduit;
-}
-
-void Stock::setNom(System::String^ nom)
-{
-    nomProduit = nom;
-}
-
-int Stock::getQuantite()
-{
-    return quantite;
-}
-
-void Stock::setQuantite(int^ extquantite)
-{
-    quantite += *extquantite;
-}
-
-float Stock::getPrix()
-{
-    return prixUnitaire;
-}
-
-void Stock::setPrix(float^ extprix)
-{
-    prixUnitaire = *extprix;
-}
-
-int Stock::getSeuil()
-{
-    return seuil;
-}
-
-void Stock::setSeuil(int^ extseuil)
-{
-    seuil = *extseuil;
-}
-
-int Stock::getTVA()
-{
-    return TVApercent;
-}
-
-void Stock::setTVA(int^ extTVA)
-{
-    TVApercent = *extTVA;
-}
-
 void Stock::ajouter(System::String^ nomProduit, System::String^ quantite, System::String^ seuil, System::String^ prixUnitaire, System::String^ TVApercent)
 {
     //Source de la bdd, puis instanciation de la requete
@@ -71,10 +16,10 @@ void Stock::ajouter(System::String^ nomProduit, System::String^ quantite, System
     System::Data::SqlClient::SqlCommand^ commande = gcnew System::Data::SqlClient::SqlCommand(requete, connexion);
 
     commande->Parameters->AddWithValue("@Libelle", nomProduit);
-    commande->Parameters->AddWithValue("@Quantite", System::Convert::ToInt32(quantite));
-    commande->Parameters->AddWithValue("@Seuil", System::Convert::ToInt32(seuil));
+    commande->Parameters->AddWithValue("@Quantite", System::Convert::ToInt64(quantite));
+    commande->Parameters->AddWithValue("@Seuil", System::Convert::ToInt64(seuil));
     commande->Parameters->AddWithValue("@prixUHT", System::Convert::ToDecimal(prixUnitaire));
-    commande->Parameters->AddWithValue("@TVA", System::Convert::ToInt32(TVApercent));
+    commande->Parameters->AddWithValue("@TVA", System::Convert::ToInt64(TVApercent));
     
     //essai de la requete plus gestion de l'Exception.
     try
@@ -93,7 +38,7 @@ void Stock::supprimer(System::String^ IDproduit)
 {
     //Source de la bdd, puis instanciation de la requete
     System::String^ connexionSource = "Data Source=.;Initial Catalog=POO;Integrated Security=True";
-    System::String^ requete = System::IO::File::ReadAllText("AjouterStock.sql");
+    System::String^ requete = System::IO::File::ReadAllText("RetirerStock.sql");
 
     //Assignation de la requete et la Source à la commande de Connexion
     System::Data::SqlClient::SqlConnection^ connexion = gcnew System::Data::SqlClient::SqlConnection(connexionSource);
@@ -114,12 +59,61 @@ void Stock::supprimer(System::String^ IDproduit)
     }
 }
 
-void Stock::afficher(System::String^ extID)
+void Stock::afficher(System::String^ IDproduit, System::String^ nomProduit, System::String^ quantite, System::String^ seuil, System::String^ prixUnitaire, System::String^ TVApercent)
 {
-    
+    //Source de la bdd, puis instanciation de la requete
+    System::String^ connexionSource = "Data Source=.;Initial Catalog=POO;Integrated Security=True";
+    System::String^ requete = System::IO::File::ReadAllText("AfficherStock.sql");
+
+    //Assignation de la requete et la Source à la commande de Connexion
+    System::Data::SqlClient::SqlConnection^ connexion = gcnew System::Data::SqlClient::SqlConnection(connexionSource);
+    System::Data::SqlClient::SqlCommand^ commande = gcnew System::Data::SqlClient::SqlCommand(requete, connexion);
+
+    commande->Parameters->AddWithValue("@Ref", System::Convert::ToInt32(IDproduit));
+
+    //essai de la requete plus gestion de l'Exception.
+    try
+    {
+        connexion->Open();
+        //Ici on récupère le résultat dans un dataReader
+        System::Data::IDataReader^ dataR = commande->ExecuteReader();
+        quantite = System::Convert::ToString(dataR);
+        connexion->Close();
+    }
+    catch (System::Exception^ ex)
+    {
+        System::Windows::Forms::MessageBox::Show(ex->Message);
+    }
+
 }
 
-void Stock::modifier(System::String^, System::String^, System::String^, System::String^, System::String^, System::String^)
+void Stock::modifier(System::String^ IDproduit, System::String^ nomProduit, System::String^ quantite, System::String^ seuil, System::String^ prixUnitaire, System::String^ TVApercent)
 {
-    
+    //Source de la bdd, puis instanciation de la requete
+    System::String^ connexionSource = "Data Source=.;Initial Catalog=POO;Integrated Security=True";
+    System::String^ requete = System::IO::File::ReadAllText("ModifierStock.sql");
+
+    //Assignation de la requete et la Source à la commande de Connexion
+    System::Data::SqlClient::SqlConnection^ connexion = gcnew System::Data::SqlClient::SqlConnection(connexionSource);
+    System::Data::SqlClient::SqlCommand^ commande = gcnew System::Data::SqlClient::SqlCommand(requete, connexion);
+
+    commande->Parameters->AddWithValue("@Ref", System::Convert::ToInt32(IDproduit));
+    commande->Parameters->AddWithValue("@Libelle", System::Convert::ToString(nomProduit));
+    commande->Parameters->AddWithValue("@Quantite", System::Convert::ToInt32(quantite));
+    commande->Parameters->AddWithValue("@Seuil", System::Convert::ToInt32(seuil));
+    commande->Parameters->AddWithValue("@prixUHT", System::Convert::ToDecimal(prixUnitaire));
+    commande->Parameters->AddWithValue("@TVA", System::Convert::ToInt32(TVApercent));
+
+
+    //essai de la requete plus gestion de l'Exception.
+    try
+    {
+        connexion->Open();
+        commande->ExecuteNonQuery();
+        connexion->Close();
+    }
+    catch (System::Exception^ ex)
+    {
+        System::Windows::Forms::MessageBox::Show(ex->Message);
+    }
 }
--- FinalePOO/ClasseStock.h
+++ FinalePOO/ClasseStock.h
@@ -5,29 +5,9 @@ ref class Stock
 {
 public:
 	~Stock(); //suppression d'un produit du stock
-	int getID();
-	System::String^ getNom();
-	void setNom(System::String^);
-	int getQuantite();
-	void setQuantite(int^);
-	float getPrix();
-	void setPrix(float^);
-	int getSeuil();
-	void setSeuil(int^);
-	int getTVA();
-	void setTVA(int^);
 	void ajouter(System::String^, System::String^, System::String^, System::String^, System::String^);
 	void supprimer(System::String^);
-	void afficher(System::String^);
+	void afficher(System::String^, System::String^, System::String^, System::String^, System::String^, System::String^);
 	void modifier(System::String^, System::String^, System::String^, System::String^, System::String^, System::String^);
-
-
-private:
-	int IDproduit;
-	System::String^ nomProduit;
-	int quantite;
-	int seuil;
-	float prixUnitaire;
-	int TVApercent;
 };
 
