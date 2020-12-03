368f1ffc433966d637cf024345316be3e242bdac
Silvio <Silvio@DESKTOP-GMKS6CP>
2020-12-03 05:46:39 +0100

    Creation à vide Personne - Personnel - Client

---
+++ FinalePOO/Client.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Personne.h"
+
+ref class Client : public Personne
+{
+/*Rappel des attributs : K_ID, Nom, Prenom, Date, NbAchat
+puis  liaison à Adresse : K_ID, numero, typeDeVoie, LibelleDeVoie, Ville, codePostal, FK_IDClient*/
+public :
+	void ajouter() override;
+	void supprimer() override;
+	void afficher() override;
+	void modifier() override;
+	void ajouterAdresse();
+	void supprimerAdresse();
+	void ajouterNombreAchat();
+};
+
