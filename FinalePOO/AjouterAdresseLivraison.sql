4a9ca39b496d4e463ddb873d36d17732eebea859
Silvio <Silvio@DESKTOP-GMKS6CP>
2020-12-04 00:07:14 +0100

    cc

--- FinalePOO/AjouterAdresseLivraison.sql
+++ FinalePOO/AjouterAdresseLivraison.sql
@@ -1,2 +1,2 @@
 UPDATE POO.dbo.Commande
-SET ID_Adresse = @ID_Adresse WHERE ID = @ID AND EXISTS @ID_Adresse 
+SET ID_Adresse = @ID_Adresse WHERE ID = @ID AND EXISTS (SELECT ID FROM POO.dbo.Adresse WHERE ID = @ID_Adresse AND Numero_de_Client = @NDC);
