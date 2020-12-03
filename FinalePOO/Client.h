3e9a63f6181e57fbe6a68735c8fb95742edc2199
Silvio <Silvio@DESKTOP-GMKS6CP>
2020-12-03 09:40:19 +0100

    UPDATE Personne

--- FinalePOO/Client.h
+++ FinalePOO/Client.h
@@ -5,10 +5,10 @@ ref class Client
 /*Rappel des attributs : K_ID, Nom, Prenom, Date, NbAchat
 puis  liaison à Adresse : K_ID, numero, typeDeVoie, LibelleDeVoie, Ville, codePostal, FK_IDClient*/
 public :
-	void ajouter() override;
-	void supprimer() override;
-	void afficher() override;
-	void modifier() override;
+	void ajouter();
+	void supprimer();
+	void afficher();
+	void modifier();
 	void ajouterAdresse();
 	void supprimerAdresse();
 	void ajouterNombreAchat();
--- FinalePOO/Personnel.h
+++ FinalePOO/Personnel.h
@@ -5,9 +5,9 @@ ref class Personnel
 	/*Rappel des attributs : K_ID, Nom, Prenom, Date, FK_ID
 puis  liaison à Adresse : K_ID, numero, typeDeVoie, LibelleDeVoie, Ville, codePostal, FK_IDClient*/
 public :
-	void ajouter() override;
-	void supprimer() override;
-	void afficher() override;
-	void modifier() override;
+	void ajouter();
+	void supprimer();
+	void afficher();
+	void modifier();
 }; 
 
