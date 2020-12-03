4abac635648537960431c72af39dd9c46c1540f2
Silvio <Silvio@DESKTOP-GMKS6CP>
2020-12-03 09:41:22 +0100

    Coco

--- FinalePOO/Personnel.cpp
+++ FinalePOO/Personnel.cpp
@@ -1,1 +1,21 @@
 #include "Personnel.h"
+
+void Personnel::ajouter()
+{
+    throw gcnew System::NotImplementedException();
+}
+
+void Personnel::supprimer()
+{
+    throw gcnew System::NotImplementedException();
+}
+
+void Personnel::afficher()
+{
+    throw gcnew System::NotImplementedException();
+}
+
+void Personnel::modifier()
+{
+    throw gcnew System::NotImplementedException();
+}
